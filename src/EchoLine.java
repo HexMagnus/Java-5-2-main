43e973d7dfe9792b37da9ffd74057b8b841b7761
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2021-11-29 06:47:39 -0300

    Initial commit

---
+++ src/EchoLine.java
@@ -0,0 +1,14 @@
+	import java.util.Scanner;
+	
+	class EchoLine{
+
+	public static void main(String[] args) {
+		Scanner myScanner = new Scanner (System.in);
+		
+		System.out.println(myScanner.nextLine());
+
+	}		
+		
+}
+
+
